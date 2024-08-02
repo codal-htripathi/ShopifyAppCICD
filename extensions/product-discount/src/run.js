cf081d6667eac6713fc00f1d722ce8887ffa2222
codal-htripathi <htripathi@codal.com>
2024-08-02 14:43:15 +0530

    first commit

---
+++ extensions/product-discount/src/run.js
@@ -0,0 +1,70 @@
+import { DiscountApplicationStrategy } from "../generated/api";
+
+/**
+ * @typedef {import("../generated/api").RunInput} RunInput
+ * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ * @typedef {import("../generated/api").Target} Target
+ * @typedef {import("../generated/api").ProductVariant} ProductVariant
+ */
+
+/**
+ * @type {FunctionRunResult}
+ */
+const EMPTY_DISCOUNT = {
+  discountApplicationStrategy: DiscountApplicationStrategy.First,
+  discounts: [],
+};
+
+/**
+ * @param {RunInput} input
+ * @returns {FunctionRunResult}
+ */
+export function run(input) {
+  const discounts = [];
+
+  input.cart.lines.forEach((line) => {
+    let discountValue = 0;
+    let minimumQuantity = 0;
+
+    if (line.quantity >= 50) {
+      discountValue = 7.5;
+      minimumQuantity = 50;
+    } else if (line.quantity >= 25) {
+      discountValue = 5;
+      minimumQuantity = 25;
+    } else if (line.quantity >= 10) {
+      discountValue = 2.5;
+      minimumQuantity = 10;
+    }
+
+    if (discountValue > 0) {
+      discounts.push({
+        targets: [
+          {
+            cartLine: {
+              id: line.id,
+              quantity: line.quantity,
+            },
+          },
+        ],
+        value: {
+          percentage: {
+            value: discountValue,
+          },
+        },
+        message: `${discountValue}% off when buying at least ${minimumQuantity} items`,
+      });
+    }
+  });
+
+  if (discounts.length === 0) {
+    console.error("No cart lines qualify for volume discount.");
+    return EMPTY_DISCOUNT;
+  }
+
+  return {
+    discounts: discounts,
+    discountApplicationStrategy: DiscountApplicationStrategy.All,
+  };
+}
+
